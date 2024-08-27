d630e2bbb736e8d9253b31b33e88964d17436956
Ian McBride <imcbride@middlebury.edu>
2024-08-27 14:49:18 -0400

    Move the code to fetch departments into this project so that it no longer replies on a web service.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,15 @@
-﻿using System;
+﻿using Microsoft.Security.Application;
+using System;
 using System.IO;
 using System.Collections.Generic;
 using System.DirectoryServices;
+using System.Linq;
 using System.Text;
 using DirectoryDepartments.edu.middlebury.devweb;
 using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Schema;
+using System.Xml.XPath;
 
 namespace DirectoryDepartments
 {
@@ -12,17 +17,116 @@ namespace DirectoryDepartments
     {
         static void Main(string[] args)
         {
-            WebDirectory directory = new WebDirectory();
-            directory.Timeout = System.Threading.Timeout.Infinite;
-
-            XmlNode departments = directory.getDepartments("");
-
-            XmlDocument doc = new XmlDocument();
-            XmlDeclaration xmlDeclaration = doc.CreateXmlDeclaration("1.0", "utf-8", null);
-            XmlNode xmlRoot = doc.ImportNode(departments, true);
-            doc.InsertBefore(xmlDeclaration, doc.DocumentElement);
-            doc.AppendChild(xmlRoot);
-            doc.Save("C:\\Uploads\\DirectoryDepartments.xml");
+            Departments fetcher = new Departments();
+            XmlDocument departments = fetcher.getDepartments("");
+            departments.Save("C:\\Uploads\\DirectoryDepartments.xml");
         }
     }
+
+    class Departments
+    {
+        public Departments() { }
+
+        public struct DirectoryDepartment
+        {
+            public string name;
+            public string company;
+
+            public DirectoryDepartment(string name, string company)
+            {
+                this.name = name;
+                this.company = company;
+            }
+        }
+
+        public class DirectoryDepartmentSortComparer : IComparer<DirectoryDepartment>
+        {
+            public int Compare(DirectoryDepartment x, DirectoryDepartment y)
+            {
+                return x.name.CompareTo(y.name);
+            }
+        }
+
+        public XmlDocument getDepartments(string company)
+        {
+            XmlDocument departments = createXmlDocument("departments");
+
+            string search = String.Empty;
+
+            company = Microsoft.Security.Application.Encoder.LdapFilterEncode(company);
+
+            if (company == "")
+            {
+                search = "(&(objectClass=user)" + company + ")";
+            }
+            else
+            {
+                search = "(&((objectClass=user))(company=*" + company + "*))";
+            }
+
+            String[] view = new String[] { "department", "company", "extensionAttribute7" };
+
+            using (DirectorySearcher searcher = new DirectorySearcher(search, view))
+            {
+                searcher.PageSize = 2000;
+                using (SearchResultCollection results = searcher.FindAll())
+                {
+                    var enumerator = results.GetEnumerator();
+
+                    List<DirectoryDepartment> list = new List<DirectoryDepartment>();
+
+                    while (enumerator.MoveNext())
+                    {
+                        SearchResult result = (SearchResult)enumerator.Current;
+
+                        ResultPropertyValueCollection resultDepartments = result.Properties["department"];
+                        ResultPropertyValueCollection resultCompanies = result.Properties["company"];
+
+                        if (resultCompanies.Count > 0 && resultDepartments.Count > 0 && resultDepartments[0].ToString().Trim() != "")
+                        {
+                            DirectoryDepartment department = new DirectoryDepartment(resultDepartments[0].ToString(), resultCompanies[0].ToString());
+
+                            if (!list.Contains(department) &&
+                                (result.Properties["extensionattribute7"] == null ||
+                                 result.Properties["extensionattribute7"].Count == 0 ||
+                                 result.Properties["extensionattribute7"][0].ToString().Replace(',', ' ').Replace("\\n", "").Trim() == ""))
+                            {
+                                list.Add(department);
+                            }
+                        }
+                    }
+
+                    list.Sort(new DirectoryDepartmentSortComparer());
+
+                    foreach (DirectoryDepartment department in list)
+                    {
+                        XmlElement xmlDepartment = departments.CreateElement("department");
+                        xmlDepartment.SetAttribute("name", department.name);
+                        xmlDepartment.SetAttribute("company", department.company);
+                        departments.DocumentElement.AppendChild(xmlDepartment);
+                    }
+
+                    return departments;
+                }
+            }
+        }
+
+        private XmlDocument createXmlDocument(string root)
+        {
+            XmlDocument document = new XmlDocument();
+
+            XmlDeclaration declaration = document.CreateXmlDeclaration("1.0", "utf-8", null);
+            XmlElement r = document.CreateElement(root);
+            document.InsertBefore(declaration, document.DocumentElement);
+            document.AppendChild(r);
+
+            return document;
+        }
+
+        static void ValidationEventHandler(object sender, ValidationEventArgs e)
+        {
+            throw new XmlSchemaValidationException();
+        }
+    }
+
 }
